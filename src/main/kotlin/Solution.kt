1671fcff1ce6fa14d23508bda1cfa231d914abe0
Ohleesang <jammmmed@gmail.com>
2024-01-31 09:11:18 +0900

    간단한 문자열문제

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,2 +1,14 @@
+import java.lang.StringBuilder
+
 class Solution {
+}
+
+fun main(args: Array<String>) {
+    val s1 = readLine()!!
+    var res = StringBuilder()
+    s1.forEach {char->
+        if(char.isUpperCase()) res.append(char.lowercaseChar())
+        else res.append(char.uppercaseChar())
+    }
+    println(res)
 }
